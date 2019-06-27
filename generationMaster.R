6f49dc50c959d47e7b48e64eee75e5156bee2106
niclasms <niclas-musies@gmx.net>
2019-06-27 10:01:23 +0200

    Functions NM & Sampling Tries

--- generationMaster.R
+++ generationMaster.R
@@ -26,6 +26,7 @@ generateDataFrames = function(endpoint = "api-test2D", batchSize = 50, loops = 1
   return(outDf)
 }
 
+
 #' @description generate hypercube input variables for api requests [-5:5]
 #' @author: Felix
 #' @param batchSize: Size of df to be generated
@@ -50,4 +51,60 @@ generateInput = function(batchSize = 50, seed, dimensions) {
   }
 
   return(input)
+}
+
+
+#' @description generate hypercube grid as basis for adaptive sampling  [-5:5]
+#' @author: Niclas
+#' @param stepSize: Size of the steps for the initial grid df to be generated
+generateGrid = function(lowerBound = -5, upperBound = 5, stepSize = 0.1, dimensions) {
+  
+  if(dimensions == 3){
+    x1 = seq(lowerBound, upperBound, by = stepSize)
+    x2 = seq(lowerBound, upperBound, by = stepSize)
+    x3 = seq(lowerBound, upperBound, by = stepSize)
+    input = expand.grid(x1,x2,x3)
+    colnames(input) <- c("x1","x2","x3")
+    
+    print(paste("generating initial grid of length" , nrow(input)))
+    
+  }
+  else{
+    x1 = seq(lowerBound, upperBound, by = stepSize)
+    x2 = seq(lowerBound, upperBound, by = stepSize)
+    input = expand.grid(x1,x2)
+    colnames(input) <- c("x1","x2")
+    
+    print(paste("generating initial grid of length" , nrow(input)))
+    
+  }
+  
+  return(input)
+}
+
+
+#'@description generate initial grid with responses from api to enable efficient sampling
+#'@author: Niclas
+#'@param fun: function that needs to be approximated
+#'@param endpoint: endpoint that will be used
+#'@param batchSize: batchSize of observations to be requested from the API; default = 50
+generateInputGrid = function(endpoint, base, token, dimensions){
+  outDf = data.frame()
+  
+  #fill dataframe with request and reponse data
+  input = generateGrid(lowerBound = -5, upperBound = 5, stepSize = 2, dimensions)
+  
+  if(dimensions == 3){
+    output = cbind(input,
+                   func1 = apirequest(input = input[,1:3], func = 1, endpoint = endpoint, base = base, token = token),
+                   func2 = apirequest(input = input[,1:3], func = 2, endpoint = endpoint, base = base, token = token))
+  }
+  
+  else{
+    output = cbind(input,
+                   func1 = apirequest(input = input[,1:2], func = 1, endpoint = endpoint, base = base, token = token),
+                   func2 = apirequest(input = input[,1:2], func = 2, endpoint = endpoint, base = base, token = token))
+  }
+  outDf = rbind(outDf, output)
+  return(outDf)
 }
--- sourcer.R
+++ sourcer.R
@@ -3,14 +3,22 @@
 #source all the necessary functions for the entire script to have a coherent structure in the project
 #insert necessary functions here
 
-#source R_Client
-source("R_Client.R")
-source("generationMaster.R")
-source("visMaster.R")
-install.packages("plot3D")
+#install and load necessary packages
+#install.packages("plot3D")
 library(plot3D)
-install.packages("plot3Drgl")
+#install.packages("plot3Drgl")
 library("plot3Drgl")
+#install.packages("rgl")
+library("rgl")
+#install.packages("httr")
 library(httr)
+#install.packages("htmltools")
 library(htmltools)
+#install.packages("jsonlite")
 library(jsonlite)
+#install.packages("ecr")
+library(ecr)
+#install.packages("PrevMap")
+library(PrevMap)
+#install.packages("sf")
+library(sf)
